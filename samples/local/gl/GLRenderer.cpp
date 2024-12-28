9bbdec23c7ad22f2118feb39e873185f47fc4276
Kejin <liang.kejin@wuta-camera.com>
2024-12-28 15:16:17 +0800

    优化GLRenderer,添加测试图

--- samples/local/gl/GLRenderer.cpp
+++ samples/local/gl/GLRenderer.cpp
@@ -6,61 +6,68 @@
 #include "common/gles/GLCoord.h"
 #include "common/gles/Texture.h"
 #include "common/gles/filter/TextureFilter.h"
+#include "common/gles/filter/SharpenFilter.h"
+#include "common/gles/FramebufferPool.h"
+#include "common/AppContext.h"
+#include <opencv2/opencv.hpp>
 
 using namespace znative;
 
-ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-uint8_t pixels[100*100*4] = {
-        0, 255, 0, 255,
-        255, 255, 0, 255,
-        0, 0, 0, 255,
-        255, 0, 0, 255};
+struct GuiVars {
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    bool show_demo_window = false;
+    float alpha_percent = 1.0f;
+};
+static GuiVars guiVars;
 
-//NV21Filter nv21Filter;
-TextureFilter filter;
-Texture2D texture2D(100,100);
-GLRect rect;
+TextureFilter texFilter;
+SharpenFilter sharpenFilter;
 
-int i = 0;
-float percent = 0;
+FramebufferPool framebufferPool;
+
+ImageTexture imageTexture;
+GLRect vertexRect;
+
 void GLRenderer::onRender(int width, int height) {
-    GLUtil::clearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-    pixels[i] = 255;
-    i = (i+1)%(40000);
-    texture2D.update(pixels);
-    filter.setViewport(width, height);
-    filter.inputTexture(texture2D);
-    rect.setRect(0, 0, 500, 500);
-//    rect.scale(0.5, 0.5);
-    rect.translate(width/2 - 250, height/2 - 250);
-    rect.setRotation(0);
-    filter.setVertexCoord(rect, width, height);
-    filter.render();
+    auto &ccolor = guiVars.clear_color;
+    GLUtil::clearColor(ccolor.x, ccolor.y, ccolor.z, ccolor.w);
 
+    if (!imageTexture.valid()) {
+        std::string testImgPath = AppContext::filesDir() + "/images/lyf.jpg";
+        cv::Mat img = cv::imread(testImgPath);
+        _FATAL_IF(img.empty(), "load image failed: %s", testImgPath.c_str());
+        cv::Mat rgba;
+        cv::cvtColor(img, rgba, cv::COLOR_BGR2RGBA);
+        imageTexture.set(rgba.data, rgba.cols, rgba.rows, GL_RGBA);
+    }
+
+    Texture2D &tex = imageTexture.textureNonnull();
+
+    vertexRect = GLRect::fitCenter((float) tex.width(), (float) tex.height(), (float) width, (float) height);
+    texFilter.setVertexCoord(vertexRect, (float) width, (float) height);
+    texFilter.setTextureCoord(0, false, true);
+    texFilter.inputTexture(tex);
+    texFilter.alpha(guiVars.alpha_percent);
+    texFilter.blend(true);
+    texFilter.render();
 }
 
-bool show_demo_window = false;
-void GLRenderer::onRenderImgui(int width, int height, ImGuiIO& io) {
-    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-    if (show_demo_window)
-        ImGui::ShowDemoWindow(&show_demo_window);
+void GLRenderer::onRenderImgui(int width, int height, ImGuiIO &io) {
+    if (guiVars.show_demo_window) {
+        ImGui::ShowDemoWindow(&guiVars.show_demo_window);
+    }
 
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
     {
-        static int counter = 0;
+        ImGui::Begin("ShaderToy");
 
-        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+        // Edit bools storing our window open/close state
+        ImGui::Checkbox("Demo Window", &guiVars.show_demo_window);
 
-        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-        ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+        // Edit 1 float using a slider from 0.0f to 1.0f
+        ImGui::SliderFloat("percent", &guiVars.alpha_percent, 0.0f, 1.0f);
+        ImGui::ColorEdit3("clear color", (float *) &guiVars.clear_color); // Edit 3 floats representing a color
 
-        ImGui::SliderFloat("percent", &percent, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-//        ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-            counter++;
         ImGui::SameLine();
-        ImGui::Text("counter = %d", counter);
 
         ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
         ImGui::End();
@@ -72,6 +79,7 @@ void GLRenderer::onPostRender(int width, int height) {
 }
 
 void GLRenderer::onExit() {
-    filter.release();
-    texture2D.release();
+    imageTexture.release();
+    texFilter.release();
+    sharpenFilter.release();
 }
--- samples/local/main.cpp
+++ samples/local/main.cpp
@@ -1,8 +1,16 @@
 #include "gl/GLRenderer.h"
+#include <common/AppContext.h>
+#include <common/utils/FileUtils.h>
+
+using namespace znative;
 
 // Main code
 int main(int argc, char** argv)
 {
+    std::string filesDir = "../../../assets";
+    std::string cacheDir = "./caches";
+    FileUtils::mkDir(cacheDir.c_str());
+    znative::AppContext::initialize(filesDir, cacheDir);
     GLRenderer::run();
     return 0;
 }
