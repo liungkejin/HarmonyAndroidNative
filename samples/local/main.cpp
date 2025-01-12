cf5e460651783bafef095409fa73709a185035cf
Kejin <liungkejin@foxmail.com>
2025-01-13 02:37:44 +0800

    ING：测试windows相机枚举

--- samples/local/main.cpp
+++ samples/local/main.cpp
@@ -1,13 +1,22 @@
 #include "gl/GLRenderer.h"
 #include <common/AppContext.h>
 #include <common/utils/FileUtils.h>
-#include "test/ZTest.h"
+
+#ifdef WIN32
+#include <stringapiset.h>
+#include <Windows.h>
+#endif
 
 using namespace znative;
 
 // Main code
 int main(int argc, char** argv)
 {
+#ifdef WIN32
+    system("chcp 65001");
+    SetConsoleOutputCP(CP_UTF8);
+    setbuf(stdout, nullptr);
+#endif
     std::string filesDir = "../../../assets";
     std::string cacheDir = "./caches";
     FileUtils::mkDir(cacheDir.c_str());
--- samples/local/win32/WinCamMgr.cpp
+++ samples/local/win32/WinCamMgr.cpp
@@ -5,71 +5,152 @@
 #include "WinCamMgr.h"
 
 #include <Windows.h>
-#include <mfapi.h>
-#include <mfidl.h>
-#include <mfreadwrite.h>
-
-#pragma comment(lib, "Mfplat.lib")
-#pragma comment(lib, "Mf.lib")
-#pragma comment(lib, "mfreadwrite.lib")
-#pragma comment(lib, "mfuuid.lib")
+#include <uuids.h>
+#include <amvideo.h>
+#include <strmif.h>
+#include <vector>
 
 NAMESPACE_DEFAULT
 
-// 摄像头设备对象
-IMFActivate ** devices = nullptr;
-// 底层媒体源对象
-IMFMediaSource* source = nullptr;
-// 上层数据读取对象
-IMFSourceReader * reader = nullptr;
+std::string ConvertBSTRToString(wchar_t* bstr) {
+    if (!bstr) {
+        return "";
+    }
+    int size = WideCharToMultiByte(CP_UTF8, 0, bstr, -1, nullptr, 0, nullptr, nullptr);
+    std::string result(size - 1, '\0');
+    WideCharToMultiByte(CP_UTF8, 0, bstr, -1, &result[0], size, nullptr, nullptr);
+    return result;
+}
+
+class CamInfo {
+public:
+    void setName(const std::string &name) {
+        m_name = name;
+    }
+
+    void putResolution(int k, int v) {
+        m_res_list.emplace_back(k, v);
+    }
+
+private:
+    std::string m_name;
+    std::vector<std::pair<int, int>> m_res_list;
+};
+
+void getCamSupportResolutions(IBaseFilter *pFilter) {
+    HRESULT hr = 0;
+    std::vector<IPin *> pins;
+    IEnumPins *enumPins = nullptr;
+    pFilter->EnumPins(&enumPins);
+
+    std::vector<std::pair<int, int>> result;
+
+    while (true) {
+        IPin *pin = nullptr;
+        hr = enumPins->Next(1, &pin, nullptr);
+        if (hr != S_OK) {
+            break;
+        }
+        PIN_INFO pInfo;
+        pin->QueryPinInfo(&pInfo);
+
+        IEnumMediaTypes *emt = nullptr;
+        pin->EnumMediaTypes(&emt);
+
+
+        while (true) {
+            AM_MEDIA_TYPE *pmt = nullptr;
+            hr = emt->Next(1, &pmt, nullptr);
+            if (FAILED(hr)) {
+                break;
+            }
+
+            if ((pmt->formattype == FORMAT_VideoInfo) && (pmt->cbFormat >= sizeof(VIDEOINFOHEADER)) && pmt->pbFormat !=
+                                                                                                       nullptr) {
+                VIDEOINFOHEADER *pvih = (VIDEOINFOHEADER *) pmt->pbFormat;
+                _INFO("VIDEOINFOHEADER: bitrate: %d, source rect[%d, %d, %d, %d], target rect[%d, %d, %d, %d]",
+                      pvih->dwBitRate, pvih->rcSource.left, pvih->rcSource.top,
+                      pvih->rcSource.right, pvih->rcSource.bottom,
+                      pvih->rcTarget.left, pvih->rcTarget.top, pvih->rcTarget.right, pvih->rcTarget.bottom);
+                _INFO("VIDEOINFOHEADER: header[size: %d, width: %d, height: %d, planes: %d, bitCount: %d, compression: %d, sizeImage: %d, xPelsPerMeter: %d, yPerlsPerMeter: %d, ClrUsed: %d, ClrImportant: %d]",
+                      pvih->bmiHeader.biSize,
+                      pvih->bmiHeader.biWidth,
+                      pvih->bmiHeader.biHeight,
+                      pvih->bmiHeader.biPlanes,
+                      pvih->bmiHeader.biBitCount,
+                      pvih->bmiHeader.biCompression,
+                      pvih->bmiHeader.biSizeImage,
+                      pvih->bmiHeader.biXPelsPerMeter,
+                      pvih->bmiHeader.biYPelsPerMeter,
+                      pvih->bmiHeader.biClrUsed,
+                      pvih->bmiHeader.biClrImportant);
+            }
+
+//            if (pmt->cbFormat) {
+//                CoTaskMemFree((PVOID)pmt->pbFormat);
+//                pmt->cbFormat = 0;
+//                pmt->pbFormat = nullptr;
+//            }
+//            if (pmt->pUnk != nullptr) {
+//                pmt->pUnk->Release();
+//                pmt->pUnk = nullptr;
+//            }
+        }
+//        emt->Release();
+    }
+}
 
 void WinCamMgr::test() {
     CoInitialize(nullptr);
 
-    IMFAttributes *attributes = nullptr;
-    IMFMediaType *mediaType = nullptr;
-
-    UINT32 width, height;
-    GUID subtype;
-    UINT32 count = 0;
-    DWORD index = 0, flag = 0;
-    LONGLONG timestamp = 0;
-    bool exitFlag = false;
-
-    // 枚举设备
-    auto hr = MFCreateAttributes(&attributes, 1);
-    _FATAL_IF(FAILED(hr), "create attr error: %d", hr)
-
-    hr = attributes->SetGUID(MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE, MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID);
-    _FATAL_IF(FAILED(hr), "set guid error: %d", hr)
-
-    hr = MFEnumDeviceSources(attributes, &devices, &count);
-    _ERROR_RETURN_IF(FAILED(hr), void(), "MFEnumDeviceSources failed: %d", hr)
-
-    _INFO("Enum device sources count: %d", count);
-    hr = MFCreateDeviceSource(devices[0], &source);
-    _ERROR_RETURN_IF(FAILED(hr), void(), "MFCreateDeviceSource failed: %d", hr)
-
-    hr = MFCreateSourceReaderFromMediaSource(source, nullptr, &reader);
-    _ERROR_RETURN_IF(FAILED(hr), void(), "MFCreateSourceReaderFromMediaSource failed: %d", hr)
-
-    hr = reader->GetCurrentMediaType(MF_SOURCE_READER_FIRST_VIDEO_STREAM, &mediaType);
-    _ERROR_RETURN_IF(FAILED(hr), void(), "GetCurrentMediaType failed: %d", hr)
-
-    // 获取图像格式
-    hr = mediaType->GetGUID(MF_MT_SUBTYPE, &subtype);
-    _INFO("subtype: %s", subtype.Data4);
-    // 获取分辨率
-    hr = MFGetAttributeSize(mediaType, MF_MT_FRAME_SIZE, &width, &height);
-    _INFO("frame size: %d x %d", width, height);
-
-    attributes->Release();
-    source->Release();
-    reader->Release();
-    mediaType->Release();
-    for (int i = 0; i < count; ++i) {
-        devices[i]->Release();
+    HRESULT hr;
+    ICreateDevEnum *pDevEnum = nullptr;
+    hr = CoCreateInstance(CLSID_SystemDeviceEnum, nullptr, CLSCTX_INPROC_SERVER, IID_ICreateDevEnum,
+                          (void **) &pDevEnum);
+    if (FAILED(hr)) {
+        _ERROR("CoCreateInstance failed: %d", hr);
+        if (pDevEnum) {
+            pDevEnum->Release();
+        }
+        return;
     }
+
+    IEnumMoniker *pEnumCat = nullptr;
+    hr = pDevEnum->CreateClassEnumerator(CLSID_VideoInputDeviceCategory, &pEnumCat, 0);
+    if (FAILED(hr)) {
+        _ERROR("ICreateDevEnum->CreateClassEnumerator failed: %d", hr);
+        pDevEnum->Release();
+        return;
+    }
+
+    IMoniker *pMoniker = nullptr;
+    ULONG cFetched;
+    int index = 0;
+    while (pEnumCat->Next(1, &pMoniker, &cFetched) == S_OK) {
+        IPropertyBag *pPropBag;
+        hr = pMoniker->BindToStorage(nullptr, nullptr, IID_IPropertyBag, (void **) &pPropBag);
+        if (SUCCEEDED(hr)) {
+            IBaseFilter *pFilter = nullptr;
+            hr = pMoniker->BindToObject(nullptr, nullptr, IID_IBaseFilter, (void **) &pFilter);
+            if (FAILED(hr)) {
+                pPropBag->Release();
+                continue;
+            }
+
+            VARIANT varName;
+            VariantInit(&varName);
+            hr = pPropBag->Read(L"FriendlyName", &varName, nullptr);
+            if (SUCCEEDED(hr)) {
+                std::string name = ConvertBSTRToString(varName.bstrVal);
+
+                _INFO("camera[%d] name: %s", index, name);
+                getCamSupportResolutions(pFilter);
+            }
+        }
+        pPropBag->Release();
+        index += 1;
+    }
+    pMoniker->Release();
 }
 
 NAMESPACE_END
